82bb1822931594668ed90ad2246e24fac2a72a7f
Greengreener <36747370+Greengreener@users.noreply.github.com>
2020-09-20 12:33:36 +1000

    Initial commit

---
+++ Assets/Scripts/Fighting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighting : MonoBehaviour
+{
+    #region Variables
+    UnitInformation unitInfo;
+    [Header("Range")]
+    static float smallRange = 2;
+    static float mediumRange = 5;
+    static float bigRange = 10;
+    float unitRange;
+    #endregion
+    void Start()
+    {
+        unitInfo = GetComponent<UnitInformation>();
+        switch (unitInfo._shipType)
+        {
+            case 1:
+                unitRange = smallRange;
+                break;
+            case 2:
+                unitRange = mediumRange;
+                break;
+            case 3:
+                unitRange = bigRange;
+                break;
+        }
+    }
+
+}
---
+++ Assets/Scripts/UnitInformation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitInformation : MonoBehaviour
+{
+    #region Variables
+    [Header("ID's")]
+    public int _shipType;
+    public string team;
+    [Header("Health")]
+    static float _smallHealth = 2;
+    static float _mediumHealth = 5;
+    static float _bigHealth = 10;
+    float _maxHealth;
+    float _health;
+    #endregion
+    void Start()
+    {
+        if (team != null && _shipType < 0)
+        {
+            //Sets unit health based on ship type
+            switch (_shipType)
+            {
+                case 1:
+                    _maxHealth = _smallHealth;
+                    break;
+                case 2:
+                    _maxHealth = _mediumHealth;
+                    break;
+                case 3:
+                    _maxHealth = _bigHealth;
+                    break;
+            }
+            _health = _maxHealth;
+        }
+        if (_health == 0)
+        {
+            Debug.LogError(_health);
+        }
+    }
+}
